93ff131700705b4b951c5e88b5bb28e5129bb7b5
앙귀머랭 <156579536+osr3173@users.noreply.github.com>
2024-07-01 13:51:33 +0900

    Add files via upload

---
+++ JAVA/Java101/src/Day01/Hello.java
@@ -0,0 +1,15 @@
+package Day01;
+
+public class Hello {
+//    main이 있어야 시작 가능 (Entry Point)
+    public static void main(String[] args) {
+//    js -> new Date(), new Math()
+//    System.out.println == console.log()
+        System.out.println("Hello java!");
+//        자기이름과 자바 후기
+        System.out.println("오승록, 파이썬이랑 비슷한데 main이란 공간 추가한 느낌");
+
+    }
+}
+
+
---
+++ JAVA/Java101/src/Day01/Operator.java
@@ -0,0 +1,22 @@
+package Day01;
+
+public class Operator {
+    public static void main(String[] args) {
+        // 산술 연산자 +, -, *,  /, %
+
+        // 증감 연산자
+        // ++, --
+
+        // 비교 연산자
+        // ==, !=, <, >, <=, >=
+
+        // 논리 연산자
+        // &&(and), ||(or) !(not)
+
+        // 삼항 연산자
+        // a==b? "떡복이" : "순대"
+
+
+
+    }
+}
