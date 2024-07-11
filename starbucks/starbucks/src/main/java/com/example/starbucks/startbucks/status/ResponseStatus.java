daf7eae56328d2959c65571769feca38fb24776d
앙귀머랭 <156579536+osr3173@users.noreply.github.com>
2024-07-11 11:46:01 +0900

    Add files via upload

---
+++ starbucks/starbucks/src/main/java/com/example/starbucks/startbucks/status/ResponseStatus.java
@@ -0,0 +1,24 @@
+package com.example.starbucks.status;
+
+public enum ResponseStatus {
+    SUCCESS(200, "Success"),
+    ERROR(500, "Error"),
+    NOT_FOUND(404, "Not Found"),
+    UNAUTHORIZED(401, "Unauthorized");
+
+    private int code;
+    private String description;
+
+    ResponseStatus(int code, String description) {
+        this.code = code;
+        this.description = description;
+    }
+
+    public int getCode() {
+        return code;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
