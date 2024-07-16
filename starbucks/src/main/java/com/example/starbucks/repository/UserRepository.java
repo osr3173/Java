0d7ec796ce4592b133c54f7a339aa02a7a3d2e67
앙귀머랭 <156579536+osr3173@users.noreply.github.com>
2024-07-16 10:43:03 +0900

    Add files via upload

---
+++ starbucks/src/main/java/com/example/starbucks/repository/UserRepository.java
@@ -0,0 +1,11 @@
+package com.example.starbucks.repository;
+
+
+import com.example.starbucks.model.UserCustom;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+// Repository :DB와 연결해 주는 인터페이스
+public interface UserRepository extends JpaRepository<UserCustom, Integer> {
+    UserCustom findByUserId(String userId);
+}
